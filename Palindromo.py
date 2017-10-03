afa2ccfbc13d103a8900ff64c20460a53e1d7ba0
josegob <jose.gob@gmail.com>
2017-10-03 16:28:17 +0200

    Primera version

---
+++ Palindromo.py
@@ -0,0 +1,19 @@
+def CheckIfPalindrome(cadena):
+    lista = []
+    lista2 = []
+
+    for i in cadena:
+        lista.append(i)
+        lista2.append(i)
+
+    lista2.reverse()
+    print(lista)
+    print(lista2)
+    if (lista != lista2):
+        print("No es palindromo")
+
+    print("Es palindromo")
+
+string = input("Introduzca una cadena: ")
+CheckIfPalindrome(string)
+
