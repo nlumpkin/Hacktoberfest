523d3646e8f88a8dfe5039bb887136c6d2589ed0
Nicole Fields <nicolelumpkin@gmail.com>
2017-10-05 15:53:50 -0400

    Update Adivina.py
    I kept the code pretty much the same. I just moved some of the print statements into functions and wrapped your game in a loop so users could have the option to play multiple times without exiting the program :)

--- Adivina.py
+++ Adivina.py
@@ -2,24 +2,48 @@
 
 import random
 
-print("Bienvenido, vamos a jugar")
+def alto():
+    print('El numero introducido es mayor que el numero buscado')#too high)
+    
+def bajo():
+    print('El numero introducido es menor que el numero buscado')#too low
+    
+def repetición():
+    jugar = input("Te gustaría jugar de nuevo? Y/n\n> ")
+    if jugar.lower() == 'y':
+        jugar()
+    print('Adios')    
 
-alto = 'El numero introducido es mayor que el numero buscado'
-bajo = 'El numero introducido es menor que el numero buscado'
-x = random.randint(1, 100)
-cont = 0
-
-numero = int(input("Introduzca un numero: "))
-
-while numero != x and cont < 10:
-    if numero > x:
-        print(alto)
+def jugar():
+    print("Bienvenido, vamos a jugar")    
+    x = random.randint(1, 100)
+    cont = 0
+    numero = None
+    
+    while cont < 10 and numero != x:    
+        while True:
+            try:
+                numero = int(input("Introduzca un numero: ")) 
+                break
+            
+            except ValueError:
+                print("Debe introducir un número entero")
+                pass
+        
+        if numero > x:
+            alto()
+            cont += 1
+            
+        elif numero < x:
+            bajo()
+            cont += 1
+    
+    if cont >= 10:
+        print("Has tardado mucho, el numero era: ", x)
+        repetición()                
     else:
-        print(bajo)
-    numero = int(input("Introduzca otro numero: "))
-    cont += 1
-
-if cont >= 10:
-    print("Has tardado mucho, el numero era: ", x)
-else:
-    print("Enhorabuena, encontraste el numero")
+        print("Enhorabuena, encontraste el numero")
+        repetición()
+        
+if __name__ == '__main__':
+    jugar()
